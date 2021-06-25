29f59b4331a583529d8725e461d859c0beff9e0d
NACHIKETAKUMAR <nachiakh@gmail.com>
2021-06-25 08:21:12 +0530

    https://github.com/nachiketakumar01/java-rbr

---
+++ javafive.java
@@ -0,0 +1,9 @@
+public class javafive {
+     public static void main(String[] args) {
+         for(int i=1;i<=5;i++){
+             System.out.println("java five times");
+         }
+        
+    }
+    
+}
