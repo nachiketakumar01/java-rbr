64127c0c424f8bf5f90cfe8296fc2628bdd75d48
NACHIKETAKUMAR <nachiakh@gmail.com>
2021-07-12 14:04:44 +0530

    class  assignment01

---
+++ triangle001.java
@@ -0,0 +1,37 @@
+/*
+3. Write a program to print star pattern?
+Input n= 5;
+Output:  
+	    *
+       * *
+      * * *
+     * * * *
+    * * * * *
+*/
+    public class triangle
+{    
+public static void main(String args[])   
+{    
+//i for rows and j for columns      
+//row denotes the number of rows you want to print  
+int i, j, row = 6;       
+//Outer loop work for rows  
+for (i=0; i<row; i++)   
+{  
+//inner loop work for space      
+for (j=row-i; j>1; j--)   
+{  
+//prints space between two stars  
+System.out.print(" ");   
+}   
+//inner loop for columns  
+for (j=0; j<=i; j++ )   
+{   
+//prints star      
+System.out.print("* ");   
+}   
+//throws the cursor in a new line after printing each line  
+System.out.println();   
+}   
+}   
+}  
